4c10f0534f2a12e235f8dfa223b5049b10ee50f1
Capabay <capabay@hotmail.com>
2021-07-28 01:01:37 +0100

    string import

--- Page1.go
+++ Page1.go
@@ -1,3 +1,14 @@
+package main
+
+import(
+	//"fmt"
+	//"string"
+)
 // In this Tutorial we are gointg to get farmiliar with the Go methods available
 // for manipulating strings
 
+// 1. Func compare: this function compares two strings and return
+//an integer, the code is as follows:
+
+// GO language program using String Compare function
+func Compare(a, b string) int
