9a9979a3478456e8996ad82c2ef9e730428e1961
Capabay <capabay@hotmail.com>
2021-07-28 01:32:02 +0100

    ContainsAny function

--- Page1.go
+++ Page1.go
@@ -7,16 +7,21 @@ import (
 
 // In this Tutorial we are gointg to get farmiliar with the Go methods available
 // for manipulating strings
-// 1. Func compare: this function compares two strings and return
+// 1. Func compare: this function compares two strings and return three values
 //an integer, the code is as follows:
 
 // GO language program using String Compare function to compare 2 strings
-func compare(a, b string) int{
-	fmt.Println(strings.Compare("Nigeria", "niger"))
-	fmt.Println(strings.Compare("niger", "Nigeria"))
-}
-func main(){
-	re
+
+func main(){	
+		fmt.Println(strings.Compare("Nigeria", "niger"))
+		fmt.Println(strings.Compare("niger", "Nigeria"))	
 }
 //The strings.Compare returns 3 possible values
 // which is 0(equality), 1(greater than) or -1(less than)
+
+//Func Contains takes a string and a substring as argument
+ func Contains(s, substr) bool
+
+ //Func ContainsAny returns a bool telling if any part of the chars is contained
+ // In the string, unlike Contains that checks all character in the substring
+ func ContainsAny(s, chars string) bool
